797aa0bb2b61961a4ff525f60df0278e7916c4b9
VovaGDX <vladimir.shishkin@gmail.com>
2024-08-27 23:49:08 +0300

    Main Создал новый раздел - FAQ И написал в нём ответы на несколько вопросов

---
+++ src/.vuepress/.temp/pages/features/9_FAQ.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/StormBPMN/stormbpmn-documentation/src/.vuepress/.temp/pages/features/9_FAQ.html.vue"
+const data = JSON.parse("{\"path\":\"/features/9_FAQ.html\",\"title\":\"FAQ - ответы на часто задаваемые вопросы\",\"lang\":\"ru-RU\",\"frontmatter\":{\"order\":9},\"headers\":[{\"level\":2,\"title\":\"Тип_элемента\",\"slug\":\"тип-элемента\",\"link\":\"#тип-элемента\",\"children\":[{\"level\":3,\"title\":\"Как изменить тип (маркер) элемента?\",\"slug\":\"как-изменить-тип-маркер-элемента\",\"link\":\"#как-изменить-тип-маркер-элемента\",\"children\":[]}]},{\"level\":2,\"title\":\"Копирование\",\"slug\":\"копирование\",\"link\":\"#копирование\",\"children\":[{\"level\":3,\"title\":\"Как скопировать элементы из одной схемы в другую?\",\"slug\":\"как-скопировать-элементы-из-однои-схемы-в-другую\",\"link\":\"#как-скопировать-элементы-из-однои-схемы-в-другую\",\"children\":[]}]},{\"level\":2,\"title\":\"Выравнивание\",\"slug\":\"выравнивание\",\"link\":\"#выравнивание\",\"children\":[{\"level\":3,\"title\":\"Как выровнять элементы по горизонтали?\",\"slug\":\"как-выровнять-элементы-по-горизонтали\",\"link\":\"#как-выровнять-элементы-по-горизонтали\",\"children\":[]},{\"level\":3,\"title\":\"Как сделать стрелки одинаковой длины?\",\"slug\":\"как-сделать-стрелки-одинаковои-длины\",\"link\":\"#как-сделать-стрелки-одинаковои-длины\",\"children\":[]}]},{\"level\":2,\"title\":\"Восстановление\",\"slug\":\"восстановление\",\"link\":\"#восстановление\",\"children\":[{\"level\":3,\"title\":\"Как восстановить процесс?\",\"slug\":\"как-восстановить-процесс\",\"link\":\"#как-восстановить-процесс\",\"children\":[]}]},{\"level\":2,\"title\":\"Cвязи\",\"slug\":\"cвязи\",\"link\":\"#cвязи\",\"children\":[{\"level\":3,\"title\":\"создание связи\",\"slug\":\"создание-связи\",\"link\":\"#создание-связи\",\"children\":[]},{\"level\":3,\"title\":\"удаление связи\",\"slug\":\"удаление-связи\",\"link\":\"#удаление-связи\",\"children\":[]}]}],\"git\":{\"createdTime\":null,\"updatedTime\":null,\"contributors\":[]},\"readingTime\":{\"minutes\":1.18,\"words\":355},\"filePathRelative\":\"features/9_FAQ.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
